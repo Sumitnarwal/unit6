07aa4aca710852131ebb543baf51c8c06dfb8708
Sumitnarwal <sumitnarwal39@gmail.com>
2022-06-29 17:18:57 +0530

    react debouncing

--- search bar country name/country/src/homePage.jsx
+++ search bar country name/country/src/homePage.jsx
@@ -4,65 +4,72 @@ import { useCallback, useContext, useState } from 'react'
 import { Link } from 'react-router-dom';
 import { saveData } from './context/saveData';
 
-export const Home=()=>{
-    const [search,setSearch]=useState([])
-    const {hansdleSetcountry}=useContext(saveData)
-//const [data,setData]=useState([])
-//console.log(data)
-const debounce=(fn)=>{
-  let timer;
-  return function(...args){
-    const context=this;
-    if(timer){
-      clearTimeout(timer)
+export const Home = () => {
+    const [search, setSearch] = useState([])
+    const { hansdleSetcountry } = useContext(saveData)
+    const [val, setVal] = useState(true)
+    //const [data,setData]=useState([])
+    //console.log(data)
+    const debounce = (fn) => {
+        let timer;
+        return function (...args) {
+            const context = this;
+            if (timer) {
+                clearTimeout(timer)
+            }
+            timer = setTimeout(() => {
+                timer = null
+                fn.apply(context, args)
+            }, 900)
+        }
     }
-    timer=setTimeout(()=>{
-      timer=null
-      fn.apply(context,args)
-    },500)
-  }
-}
 
-const handleSearch=(event)=>{
-const {value}=event.target
+    const handleSearch = (event) => {
+        const { value } = event.target
+           setVal(!val)
+        //http://localhost:8080/countrys
+        //https://demo.dataverse.org/api/search?q=${value}
 
-//http://localhost:8080/countrys
-//https://demo.dataverse.org/api/search?q=${value}
-
-// fetch(`https://demo.dataverse.org/api/search?q=${value}`)
-// .then(res=>res.json())
-// .then(json=>setSearch(json.data.items));
+        // fetch(`https://demo.dataverse.org/api/search?q=${value}`)
+        // .then(res=>res.json())
+        // .then(json=>setSearch(json.data.items));
 
 
-axios({
-    url:"http://localhost:8080/countrys",
-    method:"GET",
-    params:{
-      q:value,
-      _limit:5
+        axios({
+            url: "http://localhost:8080/countrys",
+            method: "GET",
+            params: {
+                q: value,
+                _limit: 5
+
+            }
+        }).then((res) => {
+            setSearch(res.data)
+            console.log(res.data)
+        })
 
     }
-  }).then((res)=>{
-          setSearch(res.data)
-          console.log(res.data)
-    })
-    
-  }
-  const optimiserVerson= useCallback(debounce(handleSearch),[])
-    return(
+    const optimiserVerson = useCallback(debounce(handleSearch), [])
+    return (
         <div className="App">
-        <h1>Find your Country</h1>
-        <input placeholder='Enter Country name' onChange={optimiserVerson} id="inp" />
-        {search?.length>0 && 
-        <div id='countrydata'>
-        {search?.map((el,i)=> <div key={i}>
-        <Link to={"/detail"} onClick={()=>hansdleSetcountry(el)}>
-        <p id='countryname'>{el.country}</p>
-        </Link>
-          </div>)}</div>
+            <h1>Find your Country</h1>
+            <input placeholder='Enter Country name' onChange={optimiserVerson} id="inp" /><br/>
+            {val? "you can see your Country here":
+            <div>
+            {search?.length > 0 &&
+                <div id='countrydata'>
+                {search?.map((el, i) => <div key={i}>
+                <Link to={"/detail"} onClick={() => hansdleSetcountry(el)}>
+                <p id='countryname'>{el.country}</p>
+                </Link>
+                </div>)}
+                
+                </div>
+            }
+            </div>
         }
         </div>
-        
-      )
-    
+
+    )
+
 }
